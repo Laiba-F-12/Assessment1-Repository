9d763a17b0e1f5071822f44e1b4fc0fefe260eef
LaibaF <arshadlaiba711@gmail.com>
2024-11-09 23:38:58 +0400

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,9 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# declare num1 and num2
+>>> num1=8
+>>> num2=10
+# print sum of num1 and num2
+>>> sum = num1 + num2
+>>> print(sum)
+18
