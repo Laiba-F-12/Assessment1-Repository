50cc1b4533234c4dcf4ad1ab47ed190951221d8b
LaibaF <arshadlaiba711@gmail.com>
2024-11-11 18:02:13 +0400

    Add files via upload

---
+++ task 3.py
@@ -0,0 +1,16 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# store the information (name, hometown, and age)
+>>> name = input("What is your name?: ")
+What is your name?: Laiba
+>>> hometown = input("What is your hometown?: ")
+What is your hometown?: Sharjah
+>>> age = int(input("How old are you?: "))
+How old are you?: 18
+# print the values separately 
+>>> print("Hello "+name)
+Hello Laiba
+>>> print("Your home "+hometown)
+Your home Sharjah
+>>> print("You are "+str(age)+" years old")
+You are 18 years old
